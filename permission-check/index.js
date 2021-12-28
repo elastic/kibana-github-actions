0ddc327575af3da825d6581e0a736c8c2bc726a6
Brian Seeders <brian.seeders@elastic.co>
2021-12-28 12:03:01 -0500

    Add permission-check action

---
+++ permission-check/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    const username = core.getInput('username') || github_1.context.actor;
+    const response = await github_1.getOctokit(process.env.GITHUB_TOKEN || '').rest.repos.getCollaboratorPermissionLevel({
+        ...github_1.context.repo,
+        username,
+    });
+    const userPermission = response.data.permission;
+    const desiredPermission = core.getInput('permission');
+    const perms = ['none', 'read', 'write', 'admin'];
+    if (perms.indexOf(desiredPermission) < 0) {
+        throw Error(`Invalid desired permission: ${desiredPermission}`);
+    }
+    if (perms.indexOf(userPermission) < perms.indexOf(desiredPermission)) {
+        throw Error(`User '${username}' must have at least permission '${desiredPermission}', had ${userPermission}`);
+    }
+}
+run().catch((error) => {
+    console.error('An error occurred', error);
+    core.setFailed(error.message);
+});
+//# sourceMappingURL=index.js.map
---
+++ permission-check/index.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+async function run() {
+  const username = core.getInput('username') || context.actor;
+  const response = await getOctokit(process.env.GITHUB_TOKEN || '').rest.repos.getCollaboratorPermissionLevel(
+    {
+      ...context.repo,
+      username,
+    },
+  );
+
+  const userPermission = response.data.permission;
+  const desiredPermission = core.getInput('permission');
+
+  const perms = ['none', 'read', 'write', 'admin'];
+
+  if (perms.indexOf(desiredPermission) < 0) {
+    throw Error(`Invalid desired permission: ${desiredPermission}`);
+  }
+
+  if (perms.indexOf(userPermission) < perms.indexOf(desiredPermission)) {
+    throw Error(
+      `User '${username}' must have at least permission '${desiredPermission}', had ${userPermission}`,
+    );
+  }
+}
+
+run().catch((error) => {
+  console.error('An error occurred', error);
+  core.setFailed(error.message);
+});
