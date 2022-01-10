33e36bbcf9a99821af51c0f2ae8e1b2f93724796
Søren Louv-Jansen <soren.louv@elastic.co>
2022-01-10 19:45:06 +0100

    Bump backport 6.1.5 (#17)

--- backport/index.js
+++ backport/index.js
@@ -1,54 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getConfig = void 0;
-const axios_1 = require("axios");
 const core = require("@actions/core");
 const exec_1 = require("@actions/exec");
 const github_1 = require("@actions/github");
 const backport_1 = require("backport");
-const getConfig = async (repoOwner, repoName, branch, accessToken) => {
-    const url = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/${branch}/.backportrc.json`;
-    const config = {
-        method: 'get',
-        url: url,
-        headers: { Authorization: `token ${accessToken}` },
-    };
-    const resp = await (0, axios_1.default)(config);
-    return resp.data;
-};
-exports.getConfig = getConfig;
 async function init() {
-    var _a;
     const { payload, repo } = github_1.context;
     if (!payload.pull_request) {
         throw Error('Only pull_request events are supported.');
     }
     const pullRequest = payload.pull_request;
-    const owner = pullRequest.user.login;
-    const branch = (_a = pullRequest === null || pullRequest === void 0 ? void 0 : pullRequest.base) === null || _a === void 0 ? void 0 : _a.ref;
-    if (!branch) {
-        throw Error("Can't determine PR base branch.");
-    }
+    const prAuthor = pullRequest.user.login;
     const accessToken = core.getInput('github_token', { required: true });
-    const commitUser = core.getInput('commit_user', { required: true });
-    const commitEmail = core.getInput('commit_email', { required: true });
-    const autoMerge = core.getInput('auto_merge', { required: true }) === 'true';
-    const autoMergeMethod = core.getInput('auto_merge_method', { required: true });
+    const commitUser = core.getInput('commit_user', { required: false });
+    const commitEmail = core.getInput('commit_email', { required: false });
+    const autoMerge = core.getInput('auto_merge', { required: false }) === 'true';
+    const autoMergeMethod = core.getInput('auto_merge_method', { required: false });
     const targetPRLabels = core
-        .getInput('target_pr_labels', { required: true })
+        .getInput('target_pr_labels', { required: false })
         .split(',')
         .map((label) => label.trim());
     await (0, exec_1.exec)(`git config --global user.name "${commitUser}"`);
     await (0, exec_1.exec)(`git config --global user.email "${commitEmail}"`);
-    const config = await (0, exports.getConfig)(repo.owner, repo.repo, branch, accessToken);
     await (0, backport_1.backportRun)({
-        ...config,
+        repoOwner: repo.owner,
+        repoName: repo.repo,
         accessToken,
-        fork: true,
         ci: true,
         pullNumber: pullRequest.number,
         targetPRLabels: targetPRLabels,
-        assignees: [owner],
+        assignees: [prAuthor],
         autoMerge: autoMerge,
         autoMergeMethod: autoMergeMethod,
     });
--- backport/index.ts
+++ backport/index.ts
@@ -1,19 +1,7 @@
-import axios, { AxiosRequestConfig } from 'axios';
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
 import { context } from '@actions/github';
-import { backportRun, ConfigFileOptions } from 'backport';
-
-export const getConfig = async (repoOwner: string, repoName: string, branch: string, accessToken: string) => {
-  const url = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/${branch}/.backportrc.json`;
-  const config: AxiosRequestConfig = {
-    method: 'get',
-    url: url,
-    headers: { Authorization: `token ${accessToken}` },
-  };
-  const resp = await axios(config);
-  return resp.data as ConfigFileOptions;
-};
+import { backportRun } from 'backport';
 
 async function init() {
   const { payload, repo } = context;
@@ -23,38 +11,28 @@ async function init() {
   }
 
   const pullRequest = payload.pull_request;
-  const owner: string = pullRequest.user.login;
-
-  const branch: string = pullRequest?.base?.ref;
-
-  if (!branch) {
-    throw Error("Can't determine PR base branch.");
-  }
-
+  const prAuthor: string = pullRequest.user.login;
   const accessToken = core.getInput('github_token', { required: true });
-  const commitUser = core.getInput('commit_user', { required: true });
-  const commitEmail = core.getInput('commit_email', { required: true });
-
-  const autoMerge = core.getInput('auto_merge', { required: true }) === 'true';
-  const autoMergeMethod = core.getInput('auto_merge_method', { required: true });
+  const commitUser = core.getInput('commit_user', { required: false });
+  const commitEmail = core.getInput('commit_email', { required: false });
+  const autoMerge = core.getInput('auto_merge', { required: false }) === 'true';
+  const autoMergeMethod = core.getInput('auto_merge_method', { required: false });
   const targetPRLabels = core
-    .getInput('target_pr_labels', { required: true })
+    .getInput('target_pr_labels', { required: false })
     .split(',')
     .map((label) => label.trim());
 
   await exec(`git config --global user.name "${commitUser}"`);
   await exec(`git config --global user.email "${commitEmail}"`);
 
-  const config = await getConfig(repo.owner, repo.repo, branch, accessToken);
-
   await backportRun({
-    ...config,
+    repoOwner: repo.owner,
+    repoName: repo.repo,
     accessToken,
-    fork: true,
     ci: true,
     pullNumber: pullRequest.number,
     targetPRLabels: targetPRLabels,
-    assignees: [owner],
+    assignees: [prAuthor],
     autoMerge: autoMerge,
     autoMergeMethod: autoMergeMethod,
   });
